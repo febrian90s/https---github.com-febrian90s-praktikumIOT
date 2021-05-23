72baa8552768d83e7bc28a4d6c8e2c863ad908f3
Febrian90s <febrian90s@example.com>
2021-05-24 05:33:54 +0800

    membuat kode relay terhubung ke broker

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 const char* WIFI_SSID = "realme 6";
 const char* WIFI_PASS = "";
 const char* HOSTNAME = "TestESP";
+#define PIN_RELAY 32
 
 OneWire oneWire(4);
 DallasTemperature sensors(&oneWire);
@@ -33,13 +34,31 @@ float getAmbientTemperature()
   }
 }
 
+void setRelay(bool state)
+{
+  digitalWrite(PIN_RELAY, state);
+  Serial.print("Relay state changed to: "); 
+  Serial.print(state);
+  Serial.println();
+}
+
+bool getRelay()
+{
+  bool state = digitalRead(PIN_RELAY);
+  Serial.print("Relay state is: "); 
+  Serial.print(state);
+  Serial.println();
+  return state;
+}
+
 void setup() {
   Serial.begin(115200);
-  pinMode()
+  pinMode(PIN_RELAY,OUTPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   WiFi.setHostname(HOSTNAME);
+  
   
   Serial.print("Connecting to WiFi");
   while(WiFi.status() != WL_CONNECTED) 
@@ -52,10 +71,28 @@ void setup() {
 
   sensors.begin();
 
+  iot.begind(IOTBROKER, net);
+  iot.onMesseage(messeageReceived);
+
+  Serial.print("connectig to IoT Broker");
+  while (iot.connect("ESP32","public","pubic"));
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println("IoT Broker connected succesfully.");
+
+  iot.subscribe("undiknas/ti/kelompok0/relay");
+  iot.subscribe("undiknas/ti/+/chatroom");
+
+  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   float suhu = getAmbientTemperature();
   delay(3000);
+  Client loop();
 } 
